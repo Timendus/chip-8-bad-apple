06b6ed24e6ea0b929463061c8f9aaba8da8cd8ff
Timendus <mail@timendus.com>
2022-09-20 17:21:22 +0200

    Implement bounding box encoding

--- encoder/index.js
+++ encoder/index.js
@@ -12,7 +12,7 @@ const PNG = require('pngjs').PNG;
 const rleEncode = require('./rle.js');
 
 const FRAME_START   = 1;
-const FRAME_END     = 1400;
+const FRAME_END     = 1600;
 const FRAME_STEP    = 2;
 const TARGET_WIDTH  = 48;   // Should be divisible by 8
 const TARGET_HEIGHT = 32;
@@ -42,8 +42,14 @@ for ( const f of Object.keys(movie) ) {
   frame.RLE = rleEncode(frame.input);
   if ( frame.RLE.length < frame[frame.outputType].length ) frame.outputType = 'RLE';
 
-  // frame.bbox = bboxEncode(frame.input);
-  // if ( frame.bbox.length < frame[frame.outputType].length ) frame.outputType = 'bbox';
+  frame.boundingBox = getBoundingBox(frame.input);
+  if ( frame.boundingBox.slice.length > 0 ) {
+    frame.bbox = [...frame.boundingBox.encoded, ...frame.boundingBox.slice];
+    if ( frame.bbox.length < frame[frame.outputType].length ) frame.outputType = 'bbox';
+
+    frame.bboxRLE = [...frame.boundingBox.encoded, ...rleEncode(frame.boundingBox.slice)];
+    if ( frame.bboxRLE.length < frame[frame.outputType].length ) frame.outputType = 'bboxRLE';
+  }
 
   if ( f > FRAME_START ) {
     frame.diff = frame.input.map((v, i) => v ^ movie[prev].input[i]);
@@ -51,6 +57,15 @@ for ( const f of Object.keys(movie) ) {
 
     frame.diffRLE = rleEncode(frame.diff);
     if ( frame.diffRLE.length < frame[frame.outputType].length ) frame.outputType = 'diffRLE';
+
+    frame.boundingBox = getBoundingBox(frame.diff);
+    if ( frame.boundingBox.slice.length > 0 ) {
+      frame.diffBbox = [...frame.boundingBox.encoded, ...frame.boundingBox.slice];
+      if ( frame.diffBbox.length < frame[frame.outputType].length ) frame.outputType = 'diffBbox';
+
+      frame.diffBboxRLE = [...frame.boundingBox.encoded, ...rleEncode(frame.boundingBox.slice)];
+      if ( frame.diffBboxRLE.length < frame[frame.outputType].length ) frame.outputType = 'diffBboxRLE';
+    }
 
     frame.numChangedPixels = frame.diff.reduce((a,v) =>
       v == 0 ? a : a + (nibbleLookup[v >> 4] + nibbleLookup[v & 0xF]), 0
@@ -101,16 +116,27 @@ fs.writeFileSync('player/frames.8o',
 
 console.log(`\nRENDERED ${Object.keys(movie).length} FRAMES\n`)
 
+console.log('Compression methods used:', {
+  'input':        Object.values(movie).filter(v => !v.duplicate).filter(v => v.outputType == 'input').length,
+  'bbox':         Object.values(movie).filter(v => !v.duplicate).filter(v => v.outputType == 'bbox').length,
+  'bboxRLE':      Object.values(movie).filter(v => !v.duplicate).filter(v => v.outputType == 'bboxRLE').length,
+  'RLE':          Object.values(movie).filter(v => !v.duplicate).filter(v => v.outputType == 'RLE').length,
+  'diff':         Object.values(movie).filter(v => !v.duplicate).filter(v => v.outputType == 'diff').length,
+  'diffRLE':      Object.values(movie).filter(v => !v.duplicate).filter(v => v.outputType == 'diffRLE').length,
+  'diffBbox':     Object.values(movie).filter(v => !v.duplicate).filter(v => v.outputType == 'diffBbox').length,
+  'diffBboxRLE':  Object.values(movie).filter(v => !v.duplicate).filter(v => v.outputType == 'diffBboxRLE').length,
+});
+
 const input = Object.values(movie).reduce((a, v) => a + v.input.length, 0);
 const rle = Object.values(movie).filter(v => !v.duplicate).reduce((a, v) => a + v.RLE.length, 0);
 const diffrle = Object.values(movie).filter(v => !v.duplicate).reduce((a, v) => a + (v.diffRLE ? v.diffRLE.length : v.RLE.length), 0);
 const output = Object.values(movie).filter(v => !v.duplicate).reduce((a, v) => a + v[v.outputType].length, 0);
-console.log(`${input} bytes uncompressed`);
+console.log(`\n${input} bytes uncompressed`);
 console.log(`${rle} bytes RLE encoded (${Math.round((input-rle)/input*1000)/10}% compression rate)`);
 console.log(`${diffrle} bytes RLE encoded over the diff (${Math.round((input-diffrle)/input*1000)/10}% compression rate)`);
 console.log(`${output} bytes for the chosen output (${Math.round((input-output)/input*1000)/10}% compression rate)`);
 
-const totalSize = Math.ceil(output/(FRAME_END-FRAME_START+1)*6562/2);
+const totalSize = Math.ceil(output/(FRAME_END-FRAME_START+1)*6562);
 const maxSize = 62261;
 console.log(`\nExtrapolated to 6562 frames at 15FPS, this would be ${totalSize} bytes ${totalSize > maxSize ? `(${totalSize - maxSize} bytes (${Math.round((totalSize - maxSize)/totalSize*1000)/10}%) too much)` : `-- We made it! 🎉`}`)
 
@@ -150,6 +176,49 @@ function reduceTo1Bit(image) {
     }
   }
   return data;
+}
+
+// Get the bounding box of actual data for this image, horizontally aligned to 8
+// pixels
+function getBoundingBox(image) {
+  let minX = minY = 0;
+  let maxX = TARGET_WIDTH / 8;
+  let maxY = TARGET_HEIGHT - 1;
+
+  while ( minY < maxY && image.slice(minY * TARGET_WIDTH / 8, minY * TARGET_WIDTH / 8 + TARGET_WIDTH / 8)
+                              .every(v => v == 0) ) minY += 1;
+  while ( maxY > minY && image.slice(maxY * TARGET_WIDTH / 8, maxY * TARGET_WIDTH / 8 + TARGET_WIDTH / 8)
+                              .every(v => v == 0) ) maxY -= 1;
+
+  while ( minX < maxX && image.filter((v, i) => i % (TARGET_WIDTH / 8) == minX)
+                              .every(v => v == 0) ) minX += 1;
+  while ( maxX > minX && image.filter((v, i) => i % (TARGET_WIDTH / 8) == maxX)
+                              .every(v => v == 0) ) maxX -= 1;
+
+  const slice = [];
+  if ( minX != maxX || minY != maxY ) {
+    for ( let y = minY; y <= maxY; y++ ) {
+      for ( let x = minX; x <= maxX; x++ ) {
+        slice.push(image[y * TARGET_WIDTH / 8 + x]);
+      }
+    }
+
+    assert((maxX - minX + 1) * (maxY - minY + 1) == slice.length, `slice contains the right number of bytes`);
+  }
+
+  assert((minX & 7) == minX, `minX can be encoded in 3 bits (got: ${minX})`);
+  assert((maxX & 7) == maxX, `maxX can be encoded in 3 bits (got: ${maxX})`);
+  assert((minY & 31) == minY, `minY can be encoded in 5 bits (got: ${minY})`);
+  assert((maxY & 31) == maxY, `maxY can be encoded in 5 bits (got: ${maxY})`);
+
+  return {
+    minX: minX * 8,
+    maxX: maxX * 8,
+    minY,
+    maxY,
+    encoded: [(minX << 5) + minY, (maxX << 5) + maxY],
+    slice
+  };
 }
 
 // Outputs the one bit per pixel image data from scaleAndReduce to the console.
